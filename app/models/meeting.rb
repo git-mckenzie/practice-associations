968f35c81b6968c08f0dd656d1e3fec8c653cfad
Jason McKenzie <jmac.atx@gmail.com>
2021-07-08 10:48:25 -0500

    adds create actions for speakers and meetings

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,3 +1,18 @@
 class Meeting < ApplicationRecord
   has_many :speakers, through :meeting_speaker
+
+  def create
+    meeting = Meeting.new(
+      title: params[:title],
+      agenda: params[:agenda],
+      location: params[:location],
+      time: params[:time],
+    )
+    if meeting.save
+      render json: { message: "Meeting created successfully" }, status: :created
+    else
+      render json: { errors: meeting.errors.full_messages }, status: :bad_request
+    end
+  end
+
 end
--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,3 +1,16 @@
 class Speaker < ApplicationRecord
   has_many :meetings, through :meeting_speaker
+
+  def create
+    speaker = Speaker.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+    )
+    if speaker.save
+      render json: { message: "Speaker created successfully" }, status: :created
+    else
+      render json: { errors: speaker.errors.full_messages }, status: :bad_request
+    end
+  end
 end
